ba1cf8853249e8dcb6d71effb7326f4f79639c54
Rob Wakefield <robert.g.wakefield@gmail.com>
2023-11-12 00:04:00 +0000

    Add basePath in config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
  
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
+
+  // Add basePath
+  basePath: '/thecityvarsity',
 }
 
 module.exports = nextConfig
