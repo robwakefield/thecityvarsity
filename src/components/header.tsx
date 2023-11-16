7e6d42ae3be56e1e343cd4cbdc3a5c71d25199e3
rob <rgw20@ic.ac.uk>
2023-11-16 19:21:23 +0000

    Make header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 import { NextFont } from 'next/dist/compiled/@next/font'
 
-// Displays `text` against bg color. Used at top of page
+// Displays text banner at top of screen.
 export default function Header(text: String, font: NextFont) {
-  return <nav className={"py-4 w-screen text-center text-xl text-primary bg-bg "+font.className}>
-            <b>{text.toUpperCase()}</b>
-        </nav>
+  return <nav className={
+    "py-4 px-9 w-screen text-center text-lg lg:text-xl text-primary bg-bg " + font.className
+    }>
+      <b>{text}</b>
+    </nav>
 }
