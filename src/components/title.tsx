85cdbebc5f40e67b21193166538283b98f20119b
rob <rgw20@ic.ac.uk>
2023-11-16 12:23:34 +0000

    Darken hero image

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -8,15 +8,17 @@ export default function Title(title: String, subtitle: String, buttonText: Strin
       </button>
     </div>
   // TODO: Fix responsive min height
-  return <div className="w-screen min-h-[40%] md:min-h-[50%] lg:min-h-[75%] bg-hero bg-no-repeat bg-center bg-cover flex items-center">
-      <div className={"py-12 my-4 w-full content-center text-center text-primary "+font.className}>
-        <h1 className="text-7xl">
-          <b>{title}</b>
-        </h1>
-        <h2 className="text-3xl">
-          <b>{subtitle}</b>
-        </h2>
-        {button}
-      </div>
+  return <div className="w-screen min-h-[40%] md:min-h-[50%] lg:min-h-[75%] bg-hero bg-no-repeat bg-center bg-cover">
+      <div className="h-full w-full backdrop-brightness-75 flex items-center">
+        <div className={"py-12 my-4 w-full content-center text-center text-primary "+font.className}>
+          <h1 className="text-7xl">
+            <b>{title}</b>
+          </h1>
+          <h2 className="text-3xl">
+            <b>{subtitle}</b>
+          </h2>
+          {button}
+        </div>
+      </div>  
     </div>
 }
