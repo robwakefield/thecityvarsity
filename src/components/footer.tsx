9ca00d914c7614183cfb9a493868c6e7317e88f9
rob <rgw20@ic.ac.uk>
2024-01-06 23:07:27 +0000

    Make copyright date update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
 
 export default function Footer(text: String) {
+  const date = new Date();
+  const year = date.getFullYear();
   return <div className={"w-screen bg-bg"}>
       <div className="flex flex-row py-12 px-12 text-sm text-center text-secondary justify-center">
-        <p>{"Copyright © 2023 " + text + " - All Rights Reserved."}</p>
+        <p>{"Copyright © " + year + " " + text + " - All Rights Reserved."}</p>
       </div>
     </div>
 }
