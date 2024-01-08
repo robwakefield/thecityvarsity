1c20ea9c905381e8dae6ff32a61d91ebaa71179f
rob <robert.g.wakefield@gmail.com>
2024-01-08 22:54:39 +0000

    Make Contact section responsive

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,8 +6,8 @@ import { Button } from '@/components/button'
 const archivo = Archivo_Black({ weight: "400", subsets: ["latin"] })
 
 export function Contact(text: String, address: String) {
-  return <div className="flex flex-col items-center w-screen justify-center bg-bg">
-    <div className={"py-6 max-w-4xl text-center"}>
+  return <div className="px-6 flex flex-col items-center w-screen justify-center bg-bg">
+    <div className={"py-6 max-w-3xl text-center"}>
       <h1 className={"py-4 text-4xl text-purple "+archivo.className}>CONTACT US</h1>
       <div className='py-4 text-secondary'>
         <p>{text}</p>
