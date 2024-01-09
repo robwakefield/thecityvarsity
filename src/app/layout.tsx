7cf481d7bce3a785594c19d983b85f3b01067185
rob <robert.g.wakefield@gmail.com>
2024-01-09 15:50:28 +0000

    Add PostHog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Montserrat } from 'next/font/google'
+import { Suspense } from 'react';
+import { PHProvider, PostHogPageview } from './providers';
 import { EDITION, TITLE } from '@/app/config'
 
 const montserrat = Montserrat({ weight: "400", subsets: ["latin"] })
@@ -19,11 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-        crossOrigin=""/>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""/>
       </head>
-      <body className={montserrat.className+" text-primary bg-black"}>{children}</body>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body className={montserrat.className+" text-primary bg-black"}>{children}</body>
+      </PHProvider>
     </html>
   )
 }
