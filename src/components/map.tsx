3c5cb2cd9f07bbb592858b0a9f83afc759fde526
rob <robert.g.wakefield@gmail.com>
2024-01-09 17:02:11 +0000

    Add custom capture even on map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,18 +1,27 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import { usePostHog } from 'posthog-js/react';
  
 const DynamicMap = dynamic(() => import('@/components/OpenStreetMap'), {
   ssr: false,
 })
- 
+
+
+
 export function Map(lat: number, lon: number) {
   const mapProps = {
     lat: lat,
     lon: lon,
   };
+
+  const posthog = usePostHog()
+  let onClick = () => {
+    posthog.capture('Map Clicked')
+  }
+
   return (
-    <div className='w-full'>
+    <div className='w-full' onClick={onClick}>
       <DynamicMap {...mapProps}/>
     </div>
     )
