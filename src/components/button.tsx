5690efd6d060a476d6875e3a28a2ee4ca390c5cc
rob <robert.g.wakefield@gmail.com>
2024-01-09 17:00:35 +0000

    Add custom button capture event

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,14 +2,23 @@
 
 import { useRouter } from "next/navigation";
 import { Archivo_Black } from 'next/font/google'
+import { usePostHog } from "posthog-js/react";
 
 const archivo = Archivo_Black({ weight: "400", subsets: ["latin"] })
 
 export function Button(buttonText: String, buttonLink?: string) {
   const router = useRouter()
-  let onClick = () => {}
+  const posthog = usePostHog()
+  
+  let onClick = () => {
+    posthog.capture('Button Click', {'buttonText': buttonText, 'buttonLink': buttonLink})
+  }
+  
   if (buttonLink != undefined) {
-    onClick = () => router.push(buttonLink)
+    onClick = () => {
+      posthog.capture('Button Click', {'buttonText': buttonText, 'buttonLink': buttonLink})
+      router.push(buttonLink)
+    }
   }
 
   return <div className="py-6">
