29887a78d51dc69a9d2c5b323ce8951d49685883
rob <robert.g.wakefield@gmail.com>
2024-02-04 21:14:08 +0000

    Add 64 Investments test. Update date/edition

--- src/app/config.tsx
+++ src/app/config.tsx
@@ -2,10 +2,10 @@
   IMPORTANT DATA TO UPDATE ANNUALLY
 */
 
-export const EDITION = "VI"
+export const EDITION = "VII"
 export const TITLE = "The Kinscape City Varsity " + EDITION
-export const MATCH_DATE = "11/03/23"
-export const TICKET_LINK = "https://fixr.co/event/the-city-varsity-vi-tickets-958196957"
+export const MATCH_DATE = "02/03/24"
+export const TICKET_LINK = "https://fatso.ma/ZkVF"
 
 export const AFTERPARTY_LOCATION = "Tiger Tiger London"
 export const AFTERPARTY_TEXT = "Celebrate with the players at Tiger Tiger! - our coaches will take you directly from Rosslyn to the club"
@@ -26,12 +26,13 @@ export const PARTNERS = [
     buttonLink: "https://www.kinscape.com/"
   },
   {
-    name: "iNotary", 
-    description: "iNotary Public is a flexible and modern company, whose whole ethos is based around our clients.  We have seen that notarial practice in the UK is quite dated and does not suit today’s modern business environment. We work hard to ensure that not only are our client’s requirements met,  but that this is done with the minimum fuss and distraction to our clients.",
-    image: "/thecityvarsity/partners/iNotary.webp", 
+    name: "64 Investments", 
+    description: "64 Investments is a real estate corporate finance and wealth management firm. 64 Investments has been trading as a Swiss private foundation since 2008 and was incorporated in London in 2016. Having led over Euro 4 billion of transactions, the 64 Investments team uses its strong network of relationships built over 20 years to provide bespoke, highly specialist services to its clients and partners.",
+    image: "/thecityvarsity/partners/64_Investments.png", 
     buttonText: "Learn More",
-    buttonLink: "https://www.inotarypublic.co.uk/"
-  },
+    buttonLink: "https://64invest.com/"
+  }
+  /*
   {
     name: "CORE POWER", 
     description: "CORE POWER has rapidly emerged as the global frontrunner in the development  of new nuclear technologies for the maritime sector, partnering with leading international power, engineering and nuclear innovation  companies to deliver durable zero-emission energy for floating industrial production and deep-sea shipping.",
@@ -39,6 +40,7 @@ export const PARTNERS = [
     buttonText: "Learn More",
     buttonLink: "https://corepower.energy/"
   }
+  */
 ]
 
 /*
